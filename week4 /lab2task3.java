9e4bdfea28f4297a250c50ddef320fb872ee5947
cambrian1112 <3223796079@qq.com>
2024-09-30 10:00:54 +0800

    Create lab2task3.java

---
+++ week4 /lab2task3.java
@@ -0,0 +1,8 @@
+public class lab2task3 {
+    public static void main(String[] args) {
+        int today=2;
+        int plus=100%7;
+        int day=today+plus;
+        System.err.println("the day is "+day);
+    }
+}
