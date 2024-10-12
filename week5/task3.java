efa90342437bee93c8766a767d24359d5953230b
cambrian1112 <3223796079@qq.com>
2024-10-12 11:08:13 +0800

    Create task3.java

---
+++ week5/task3.java
@@ -0,0 +1,20 @@
+public class w5task3 {
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(2.1));
+        System.out.println(Math.ceil(2.0));
+        System.out.println(Math.ceil(-2.0));
+        System.out.println(Math.ceil(-2.1));
+        System.out.println(Math.floor(2.1));
+        System.out.println(Math.floor(2.0));
+        System.out.println(Math.floor(-2.0));
+        System.out.println(Math.floor(-2.1));
+        System.out.println(Math.rint(-2.0));
+        System.out.println(Math.rint(-2.1));
+        System.out.println(Math.rint(2.0));
+        System.out.println(Math.rint(2.1));
+        System.out.println(Math.round(-2.0f));
+        System.out.println(Math.round(-2.1));
+        System.out.println(Math.round(2.0f));
+        System.out.println(Math.round(2.1));
+    }
+}
