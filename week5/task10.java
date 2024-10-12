61970ad7d32257cf0e2c0179c9da94725133ccbf
cambrian1112 <3223796079@qq.com>
2024-10-12 11:10:11 +0800

    Create task10.java

---
+++ week5/task10.java
@@ -0,0 +1,8 @@
+public class w5task10 {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        System.out.println("The message in lowecase is: "+message.toLowerCase());
+        System.out.println("The message in UPPERCASE is: "+message.toUpperCase());
+        
+}
+}
