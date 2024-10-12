b47a2173c87c86fdce50cf6e5717c516905f5cba
cambrian1112 <3223796079@qq.com>
2024-10-12 11:09:42 +0800

    Create task8.java

---
+++ week5/task8.java
@@ -0,0 +1,6 @@
+public class w5task8 {
+    public static void main(String[] args) {
+        String message="welcome to java";
+        System.out.println("the length of "+ message +" is: "+ message.length());
+    }
+}
