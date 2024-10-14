f7d550dd8b294eb86647953f0a414532b0922dcb
cambrian1112 <3223796079@qq.com>
2024-10-14 09:29:44 +0800

    Create task15.java

---
+++ week6/task15.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class w6task15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
