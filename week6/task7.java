40180cb1cbee58d6f1a695474d95027c18d93731
cambrian1112 <3223796079@qq.com>
2024-10-14 09:16:10 +0800

    Create task7.java

---
+++ week6/task7.java
@@ -0,0 +1,25 @@
+public class w6task7 {
+    public static void main(String[] args) {
+        int sum = 0; 
+        int j = 0; 
+        int i;
+
+        for (i = 0; i < 10; i++) {
+            sum += i;
+        }
+
+        if (i < j) { 
+            System.out.println(i); 
+        } else {
+            System.out.println(j); 
+        }
+
+        while (j <= 10) { 
+            j++;
+        }
+
+        do {
+            j++; 
+        } while (j <= 10);
+    }
+}
