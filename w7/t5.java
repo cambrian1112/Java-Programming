02a12874142a04ea180984f7e450a8b14d3d3f82
cambrian1112 <3223796079@qq.com>
2024-10-21 10:57:23 +0800

    Create t5.java

---
+++ w7/t5.java
@@ -0,0 +1,11 @@
+public class l4t5 {
+    public static void main(String[] args) {
+        int n=5;
+        String message="Welcome to Java";
+        nPrintfln(message,n);
+    }
+public static void nPrintfln(String message,int n){
+    for(int i=1;i<=n;i++)
+    System.out.println(message);
+}
+}
